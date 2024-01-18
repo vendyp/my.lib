e9fff0ebd99731608751159b95bf0bae6060ee9b
Vendy <dwivendypratama@gmail.com>
2024-01-18 22:21:56 +0700

    add system text json serializer

---
+++ src/MyLib.Infrastructure/Serialization/SystemTextJsonSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using MyLib.Abstractions.Serialization;
+
+namespace MyLib.Infrastructure.Serialization;
+
+internal sealed class SystemTextJsonSerializer : IJsonSerializer
+{
+    private readonly JsonSerializerOptions _options;
+
+    public SystemTextJsonSerializer(JsonSerializerOptions? options = null)
+    {
+        _options = options ?? new()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new JsonStringEnumConverter(), new DateOnlyConverter(), new TimeOnlyConverter() }
+        };
+    }
+
+    public string Serialize<T>(T value) => JsonSerializer.Serialize(value, _options);
+
+    public T? Deserialize<T>(string value) => JsonSerializer.Deserialize<T>(value, _options);
+
+    public object? Deserialize(string value, Type type) => JsonSerializer.Deserialize(value, type, _options);
+}
---
+++ tests/MyLib.Tests/Serialization/SystemTextJsonSerializerTests.cs
@@ -0,0 +1,18 @@
+﻿using MyLib.Infrastructure.Serialization;
+using Shouldly;
+
+namespace MyLib.Tests.Serialization;
+
+public class SystemTextJsonSerializerTests
+{
+    [Fact]
+    public void SystemTextJsonSerializer_Test_Serialize()
+    {
+        string expectedResult = @"{""id"":""1"",""name"":""test""}";
+
+        var service = new SystemTextJsonSerializer();
+        var result = service.Serialize(new { id = "1", name = "test" });
+
+        expectedResult.ShouldBe(result);
+    }
+}
