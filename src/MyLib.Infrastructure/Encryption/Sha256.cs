96d7cdae3ffc10cb9577526f472c416c872fe559
Vendy <dwivendypratama@gmail.com>
2024-01-19 18:19:27 +0700

    add sha256

---
+++ src/MyLib.Infrastructure/Encryption/Sha256.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using MyLib.Abstractions.Encryption;
+
+namespace MyLib.Infrastructure.Encryption;
+
+internal sealed class Sha256 : ISha256
+{
+    public string Hash(string data)
+    {
+        var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(data));
+        var builder = new StringBuilder();
+        foreach (var @byte in bytes)
+            builder.Append(@byte.ToString("x2"));
+
+        return builder.ToString();
+    }
+}
---
+++ tests/MyLib.Tests/Encryption/Sha256Tests.cs
@@ -0,0 +1,19 @@
+﻿using MyLib.Infrastructure.Encryption;
+using Shouldly;
+
+namespace MyLib.Tests.Encryption;
+
+public class Sha256Tests
+{
+    [Fact]
+    public void Sha256_Hash_Tests()
+    {
+        const string parameter = "qwerty@12345@1234";
+        const string expectedResult = "e7697ce22baf29a097b33e4fcd041946787d9be2b3d252d30f3611286320e951";
+
+        var sha256 = new Sha256();
+        var result = sha256.Hash(parameter);
+
+        result.ShouldBe(expectedResult);
+    }
+}
