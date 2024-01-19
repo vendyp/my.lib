2731aa87af0f2716b510d3a8475d86b636d864e1
Vendy <dwivendypratama@gmail.com>
2024-01-19 18:19:18 +0700

    add md5

---
+++ src/MyLib.Infrastructure/Encryption/Md5.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using MyLib.Abstractions.Encryption;
+
+namespace MyLib.Infrastructure.Encryption;
+
+internal sealed class Md5 : IMd5
+{
+    public string Hash(string value)
+    {
+        var hash = MD5.HashData(Encoding.UTF8.GetBytes(value));
+        var stringBuilder = new StringBuilder();
+        foreach (var @byte in hash)
+            stringBuilder.Append(@byte.ToString("X2"));
+
+        return stringBuilder.ToString().ToLowerInvariant();
+    }
+}
---
+++ tests/MyLib.Tests/Encryption/Md5Tests.cs
@@ -0,0 +1,19 @@
+﻿using MyLib.Infrastructure.Encryption;
+using Shouldly;
+
+namespace MyLib.Tests.Encryption;
+
+public class Md5Tests
+{
+    [Fact]
+    public void Md5_Hash_Tests()
+    {
+        const string expectedResult = "06db53e28cb810dda69d11c606aa67bc";
+        const string parameter = "12344321jffhfee12";
+        
+        var md5 = new Md5();
+        var result = md5.Hash(parameter);
+        
+        result.ShouldBe(expectedResult);
+    }
+}
