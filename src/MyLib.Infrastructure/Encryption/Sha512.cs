537690dee1ade66192afc38eba221c831f1f83b1
Vendy <dwivendypratama@gmail.com>
2024-01-19 18:19:32 +0700

    add sha512

---
+++ src/MyLib.Infrastructure/Encryption/Sha512.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using MyLib.Abstractions.Encryption;
+
+namespace MyLib.Infrastructure.Encryption;
+
+internal sealed class Sha512 : ISha512
+{
+    public string Hash(string data)
+    {
+        var bytes = SHA512.HashData(Encoding.UTF8.GetBytes(data));
+        var builder = new StringBuilder();
+        foreach (var @byte in bytes)
+            builder.Append(@byte.ToString("x2"));
+
+        return builder.ToString();
+    }
+}
---
+++ tests/MyLib.Tests/Encryption/Sha512Tests.cs
@@ -0,0 +1,19 @@
+﻿using MyLib.Infrastructure.Encryption;
+using Shouldly;
+
+namespace MyLib.Tests.Encryption;
+
+public class Sha512Tests
+{
+    [Fact]
+    public void Sha512_Hash_Tests()
+    {
+        const string parameter = "qwerty@1234$#21";
+        const string expectedResult = "69d8b7a7b15fd40d533359025599c8c45a96197f729ceee3e79b179232dc288eef04e85ae0c21833120f4445147b8ffd560ddc4c58d0a5593c81be9edd89a47b";
+
+        var sha512 = new Sha512();
+        var result = sha512.Hash(parameter);
+        
+        result.ShouldBe(expectedResult);
+    }
+}
