fe5c9228e73c23b079db02d9fc5c9952a4f5b949
Vendy <dwivendypratama@gmail.com>
2024-01-22 23:00:06 +0700

    add base pagination class

---
+++ src/MyLib.Abstractions/Queries/BasePagination.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLib.Abstractions.Queries;
+
+public abstract class BasePagination
+{
+    public int Page { get; set; } = 1;
+    public int Size { get; set; } = 10;
+    public string? OrderBy { get; set; }
+    public string? OrderType { get; set; }
+    public int CalculateSkip() => (Page - 1) * Size;
+}
